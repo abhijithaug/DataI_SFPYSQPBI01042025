5b972ea7f73de1390d5a5bf198032e7bb3c33c82
ABHIJITH AUGUSTINE <137610807+abhijithaug@users.noreply.github.com>
2025-04-01 16:31:26 +1000

    Create Table search.sql

---
+++ Initial Layer/Table search.sql
@@ -0,0 +1,14 @@
+select * from [initialSFPYSQPBI].[DimCustomerData];
+GO
+select * from [initialSFPYSQPBI].[DimDate];
+GO
+select * from [initialSFPYSQPBI].[DimLoyaltyInfo];
+GO
+select * from [initialSFPYSQPBI].[DimProductData];
+GO
+select * from [initialSFPYSQPBI].[DimStoreData];
+GO
+select * from [initialSFPYSQPBI].[factOrders];
+GO
+
+[DataI_SFPYSQPBI01042025]
